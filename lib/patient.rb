a36e259f7cd40d11e01d43e2ce01cbb1ea26d871
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 20:49:57 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient 
+  
+  attr_reader :name 
+  @@all 
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def new_appointment(doctor, date)
+    Appoinment.new()
+  end 
+  
+  def appointments
+    Appoinment.all.select {|appt| appt.patient == self}
+  end 
+  
+  def doctors
+    appointments.map {|appt| appt.doctor} 
+  end 
+  
+end 
