229e063b72bd0522ba147b9d89aecfdc5340cad1
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 20:51:40 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment 
+  
+  attr_reader 
+  @@all 
+  def self.all 
+    @@all
+  end 
+  
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient 
+    @doctor = doctor 
+    @@all << self 
+  end 
+  
+end 
