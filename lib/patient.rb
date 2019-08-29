40d4121a86a4f1b849890b6e59c17e2b26eb91e0
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 20:54:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end 
   
   def appointments
-    Appoinment.all.select {|appt| appt.patient == self}
+    Appointment.all.select {|appt| appt.patient == self}
   end 
   
   def doctors
